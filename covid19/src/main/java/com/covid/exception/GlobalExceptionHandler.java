5fcf9495abdd1fa3a8ffee177965f631adddab3f
shoto2000 <tayyabsyed2000@gmail.com>
2022-10-01 19:08:57 +0530

    login signup module

---
+++ covid19/src/main/java/com/covid/exception/GlobalExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.covid.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(SigninException.class)
+	public ResponseEntity<ErrorDetails> signinExceptionHandler(SigninException e, WebRequest req) {
+
+		ErrorDetails err = new ErrorDetails(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
+
+		return new ResponseEntity<ErrorDetails>(err,HttpStatus.BAD_REQUEST);
+
+	}
+	@ExceptionHandler(LoginException.class)
+	public ResponseEntity<ErrorDetails> loginExceptionHandler(LoginException e, WebRequest req) {
+		
+		ErrorDetails err = new ErrorDetails(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err,HttpStatus.BAD_REQUEST);
+		
+	}
+	@ExceptionHandler(LogoutException.class)
+	public ResponseEntity<ErrorDetails> logoutExceptionHandler(LogoutException e, WebRequest req) {
+		
+		ErrorDetails err = new ErrorDetails(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<ErrorDetails>(err,HttpStatus.BAD_REQUEST);
+		
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<ErrorDetails> ExceptionHandler(Exception e, WebRequest req) {
+
+		ErrorDetails err = new ErrorDetails(LocalDateTime.now(), e.getMessage(), req.getDescription(false));
+
+		return new ResponseEntity<ErrorDetails>(err,HttpStatus.INTERNAL_SERVER_ERROR);
+
+	}
+}
---
+++ covid19/src/main/java/com/covid/model/UserLogin.java
@@ -0,0 +1,11 @@
+package com.covid.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class UserLogin {
+	private String mobileNo;
+	private String password;
+}
